f3eaee90f6cf1ed371d5fa8c8cb35c6b2d6897bf
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-10-02 14:59:46 +0700

    Add font argument

--- change.py
+++ change.py
@@ -6,17 +6,19 @@ from docx import Document
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help="Input path file")
 parser.add_argument("-o", "--output", help="Output path file")
+parser.add_argument("-f", "--font", help="Font name", default="Arial")
 
 args = parser.parse_args()
 
 inputFile = args.input
 outputFile = args.output
+fontName = args.font
 
 document = Document(inputFile)
 
 for paragraph in document.paragraphs:
     for run in paragraph.runs:
-        run.font.name = 'Arial'
+        run.font.name = fontName
 
 if not outputFile:
 	basename = os.path.basename(inputFile)
