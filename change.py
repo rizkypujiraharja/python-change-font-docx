d9f9ddfd164b6156b65f3ea07f21897b2f484633
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-10-02 14:46:41 +0700

    first commit

---
+++ change.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+
+from docx import Document
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="Input path file")
+parser.add_argument("-o", "--output", help="Output path file")
+
+args = parser.parse_args()
+
+inputFile = args.input
+outputFile = args.output
+
+document = Document(inputFile)
+
+for paragraph in document.paragraphs:
+    for run in paragraph.runs:
+        run.font.name = 'Arial'
+
+if not outputFile:
+	basename = os.path.basename(inputFile)
+	dirname = os.path.dirname(inputFile)
+	if not dirname:
+		outputFile = "result_"+basename
+	else:
+		outputFile = dirname+"/result_"+basename
+
+document.save(outputFile)
+print("Result: " + outputFile)
